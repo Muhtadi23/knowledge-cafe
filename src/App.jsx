63b923661c4b852379c75f23abdc46dd73c59c63
Muhtadi23 <miranmuhtadi121@gmail.com>
2023-09-12 01:44:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-6xl bg-red-500'>Knowladge Cafe</h1>
+    </>
+  )
+}
+
+export default App
